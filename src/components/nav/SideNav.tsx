87013099860eae064dc04dd9aca297231adb501d
matejkostov <matej4o9@gmail.com>
2024-02-07 12:04:17 +0200

    remove dnd on SideNav

--- src/components/nav/SideNav.tsx
+++ src/components/nav/SideNav.tsx
@@ -5,8 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { UIContext } from "../../context/UIProvider";
 import ConfirmationModal from "../ConfirmationModal";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-// import useDragAndDrop from "../../hooks/useDragAndDrop";
 
 const SideNav = ({ isSideNavOpened, toggleSideNav }: ComponentWithSideNav) => {
   const [newCategoryName, setNewCategoryName] = useState<string>("");
@@ -18,7 +16,6 @@ const SideNav = ({ isSideNavOpened, toggleSideNav }: ComponentWithSideNav) => {
   const { categories, currentCategory, addNewCategory, deleteCategory } =
     useContext(TodoContext);
   const navigate = useNavigate();
-  // const { onDragEnd } = useDragAndDrop();
 
   const upcomingTodos = categories
     ? categories.reduce((accumulator, category) => {
@@ -275,142 +272,87 @@ const SideNav = ({ isSideNavOpened, toggleSideNav }: ComponentWithSideNav) => {
               </div>
             )}
           </div>
-          <DragDropContext
-            onDragEnd={() =>
-              // onDragEnd(result, editCategory, moveTodo, currentCategory)
-              console.log("kur")
-            }
-          >
-            <Droppable droppableId="categories">
-              {(provided) => (
-                <ul
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  className="space-y-2 py-2 font-medium border-t border-gray-200 dark:border-gray-700"
+          <ul className="space-y-2 py-2 font-medium border-t border-gray-200 dark:border-gray-700">
+            {categories?.map((category) => (
+              <li
+                key={category.id}
+                onClick={() => handleCategoryOpen(category)}
+              >
+                <a
+                  className={`flex cursor-pointer items-center p-2 text-gray-900 transition duration-75 rounded-lg ${" hover:bg-gray-100 dark:hover:bg-gray-650"}  dark:text-white group`}
                 >
-                  {categories?.map((category, index) => (
-                    <Draggable
-                      key={category.id}
-                      draggableId={category?.id || ""}
-                      index={index}
+                  {isEditClicked && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke={isDarkMode ? "white" : "black"}
+                      className="w-4 h-4 mr-1.5"
+                      onClick={() => setIsDeleteCategoryClicked(true)}
                     >
-                      {(provided) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                        >
-                          <li onClick={() => handleCategoryOpen(category)}>
-                            <a
-                              className={`flex cursor-pointer items-center p-2 text-gray-900 transition duration-75 rounded-lg ${" hover:bg-gray-100 dark:hover:bg-gray-650"}  dark:text-white group`}
-                            >
-                              {isEditClicked && (
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke={isDarkMode ? "white" : "black"}
-                                  className="w-4 h-4 mr-1.5"
-                                  onClick={() =>
-                                    setIsDeleteCategoryClicked(true)
-                                  }
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                  />
-                                </svg>
-                              )}
-                              <span className="flex-1 whitespace-nowrap">
-                                {category.name}
-                              </span>
-                              {isEditClicked ? (
-                                <div {...provided.dragHandleProps}>
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-5 h-5"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                    />
-                                  </svg>
-                                </div>
-                              ) : (
-                                <span
-                                  className={`inline-flex items-center justify-center px-1.5 py-0.5 ms-3 text-xs font-medium text-gray-800 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-gray-300`}
-                                >
-                                  {categories
-                                    ? category.subCategories?.reduce(
-                                        (accumulator, subCategory) => {
-                                          return (
-                                            accumulator +
-                                            subCategory?.todos?.length
-                                          );
-                                        },
-                                        0
-                                      )
-                                    : []}
-                                </span>
-                              )}
-                            </a>
-                            <ConfirmationModal
-                              key={category.id}
-                              isOpen={isDeleteCategoryClicked}
-                              onRequestClose={() =>
-                                setIsDeleteCategoryClicked(false)
-                              }
-                              onConfirm={() => confirmDelete(category)}
-                              title="Confirm Deletion"
-                              message={`Are you sure you want to delete this category (${category.name})?`}
-                            />
-                          </li>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-
-                  {isNewCategoryClicked ? (
-                    <li>
-                      <a className="flex cursor-pointer items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-650 dark:text-white group">
-                        <div className="text-gray-900 dark:text-white ">
-                          <input
-                            ref={inputRef}
-                            name="todo-label"
-                            placeholder="Category..."
-                            className={`cursor-pointer bg-transparent overflow-hidden outline-none w-32`}
-                            value={newCategoryName}
-                            onChange={(e) => setNewCategoryName(e.target.value)}
-                            onBlur={handleCategoryCreate}
-                            onKeyDown={handleEnterKey}
-                          />
-                        </div>
-                      </a>
-                    </li>
-                  ) : (
-                    // !snapshot.isDraggingOver && (
-                    <div
-                      className="cursor-pointer flex justify-center p-2"
-                      onClick={() => setIsNewCategoryClicked(true)}
-                    >
-                      <PlusIcon
-                        height={20}
-                        color={isDarkMode ? "white" : "black"}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                       />
-                    </div>
-                    // )
+                    </svg>
                   )}
-                </ul>
-              )}
-            </Droppable>
-          </DragDropContext>
+                  <span className="flex-1 whitespace-nowrap">
+                    {category.name}
+                  </span>
+                  {!isEditClicked && (
+                    <span
+                      className={`inline-flex items-center justify-center px-1.5 py-0.5 ms-3 text-xs font-medium text-gray-800 bg-gray-300 rounded-full dark:bg-gray-700 dark:text-gray-300`}
+                    >
+                      {categories
+                        ? category.subCategories?.reduce(
+                            (accumulator, subCategory) => {
+                              return accumulator + subCategory?.todos?.length;
+                            },
+                            0
+                          )
+                        : []}
+                    </span>
+                  )}
+                </a>
+                <ConfirmationModal
+                  key={category.id}
+                  isOpen={isDeleteCategoryClicked}
+                  onRequestClose={() => setIsDeleteCategoryClicked(false)}
+                  onConfirm={() => confirmDelete(category)}
+                  title="Confirm Deletion"
+                  message={`Are you sure you want to delete this category (${category.name})?`}
+                />
+              </li>
+            ))}
+
+            {isNewCategoryClicked ? (
+              <li>
+                <a className="flex cursor-pointer items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-650 dark:text-white group">
+                  <div className="text-gray-900 dark:text-white ">
+                    <input
+                      ref={inputRef}
+                      name="todo-label"
+                      placeholder="Category..."
+                      className={`cursor-pointer bg-transparent overflow-hidden outline-none w-32`}
+                      value={newCategoryName}
+                      onChange={(e) => setNewCategoryName(e.target.value)}
+                      onBlur={handleCategoryCreate}
+                      onKeyDown={handleEnterKey}
+                    />
+                  </div>
+                </a>
+              </li>
+            ) : (
+              <div
+                className="cursor-pointer flex justify-center p-2"
+                onClick={() => setIsNewCategoryClicked(true)}
+              >
+                <PlusIcon height={20} color={isDarkMode ? "white" : "black"} />
+              </div>
+            )}
+          </ul>
         </div>
       </aside>
     </div>
